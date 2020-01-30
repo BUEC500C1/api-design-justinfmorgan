bcf2ab48df1610155de73ce5938d062a8404c7b9
justinfmorgan <justinfmorgan@gmail.com>
2020-01-29 21:16:42 -0500

    Finished setting up the hello tweepy

---
+++ helloTweepy.py
@@ -0,0 +1,12 @@
+import tweepy
+
+import keys
+
+auth = tweepy.OAuthHandler(keys.consumer_key, keys.consumer_secret)
+auth.set_access_token(keys.access_token, keys.access_token_secret)
+
+api = tweepy.API(auth)
+
+public_tweets = api.home_timeline()
+for tweet in public_tweets:
+    print(tweet.text)
