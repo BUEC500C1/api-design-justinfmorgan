7616334614e292cec3da49724ed72c36d8ec7ef0
justinfmorgan <justinfmorgan@gmail.com>
2020-02-08 17:05:06 -0500

    added further error checking and removed unused libraries

--- helloTweepy.py
+++ helloTweepy.py
@@ -8,16 +8,5 @@ auth.set_access_token(keys.access_token, keys.access_token_secret)
 api = tweepy.API(auth)
 
 public_tweets = api.home_timeline()
-
-# for tweet in public_tweets:
-#     print(tweet.text)
-
-# for tweet in public_tweets:
-#     print(tweet._json['entities']['media'][0]['media_url_https'])
-
-for tweet in tweepy.Cursor(api.search, q='tweepy cat').items(10):
-    try:
-        print(tweet._json['entities']['media'][0]['media_url_https'])
-    except:
-        pass
-
+for tweet in public_tweets:
+    print(tweet.text)
