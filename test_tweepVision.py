33058e166ccdc65e7f5150a7e0bf7916ca3aa23f
Justin Morgan <justinfmorgan@gmail.com>
2020-02-10 17:48:10 -0500

    Update test_tweepVision.py
    Had to remove all tests dependent on keys. Also had to remove the unused imports since flake8 was failing.

--- test_tweepVision.py
+++ test_tweepVision.py
@@ -1,9 +1,9 @@
-from tweepVisionApi import visionAnalysis
+#from tweepVisionApi import visionAnalysis
 #, searchTwitter, searchAndAnalyzeImage
 from main import main
 
 def test_apiUnitTest():
-    assert visionAnalysis("BADFILE") == -1
+    #assert visionAnalysis("BADFILE") == -1
     #assert searchTwitter(1124871874501958198590218) == -1
     #assert searchAndAnalyzeImage("129r01*U)FWEJF)*EWJF") == -1
     assert main() == -1 #Test to make sure when no arguments an error occurs
