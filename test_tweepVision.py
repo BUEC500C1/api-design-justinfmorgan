cc4e455dbefdad68ee7d6203536d81106873f8f4
justinfmorgan <justinfmorgan@gmail.com>
2020-02-09 23:15:43 -0500

    Finished up error checking tests

--- test_tweepVision.py
+++ test_tweepVision.py
@@ -1,3 +1,8 @@
+from tweepVisionApi import visionAnalysis, searchTwitter, searchAndAnalyzeImage
+from main import main
+
 def test_apiUnitTest():
-    assert exec("main.py") == "Please provide a search term argument in quotes"
-    
+    assert visionAnalysis("BADFILE") == -1
+    assert searchTwitter(1124871874501958198590218) == -1
+    assert searchAndAnalyzeImage("129r01*U)FWEJF)*EWJF") == -1
+    assert main() == -1 #Test to make sure when no arguments an error occurs
